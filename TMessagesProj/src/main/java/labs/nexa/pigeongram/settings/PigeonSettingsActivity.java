41553a4101dc72c5215091686d40d51478d1f5c0
wezqq <168109017+wezzqq@users.noreply.github.com>
2026-05-09 14:11:52 +0500

    Refactor PigeonSettingsActivity to use UniversalAdapter

--- TMessagesProj/src/main/java/labs/nexa/pigeongram/settings/PigeonSettingsActivity.java
+++ TMessagesProj/src/main/java/labs/nexa/pigeongram/settings/PigeonSettingsActivity.java
@@ -1,53 +1,71 @@
 package labs.nexa.pigeongram.settings;
 
 import android.content.Context;
-import android.view.Gravity;
-import android.widget.FrameLayout;
+import android.view.View;
 
+import org.telegram.messenger.LocaleController;
 import org.telegram.messenger.R;
 import org.telegram.ui.ActionBar.BaseFragment;
-import org.telegram.ui.Cells.TextCheckCell;
+import org.telegram.ui.Components.UItem;
+import org.telegram.ui.Components.UniversalAdapter;
+
+import java.util.ArrayList;
 
 import labs.nexa.pigeongram.config.PigeonConfig;
 
 public class PigeonSettingsActivity extends BaseFragment {
 
+    private UniversalAdapter adapter;
+
+    private final int hideStoriesRow = 1;
+
     @Override
-    public android.view.View createView(Context context) {
+    public View createView(Context context) {
 
         actionBar.setBackButtonImage(R.drawable.ic_ab_back);
         actionBar.setTitle("PigeonGram Settings");
 
-        FrameLayout frameLayout = new FrameLayout(context);
-
-        TextCheckCell hideStoriesCell = new TextCheckCell(context);
-        hideStoriesCell.setTextAndCheck(
-                "Hide Stories",
-                PigeonConfig.hideStories(),
-                false
+        adapter = new UniversalAdapter(
+                null,
+                this::fillItems,
+                this::onItemClick,
+                null
         );
 
-        hideStoriesCell.setOnClickListener(v -> {
+        fragmentView = adapter.createView(context);
+
+        return fragmentView;
+    }
+
+    private void fillItems(ArrayList<UItem> items,
+                           UniversalAdapter adapter) {
+
+        items.add(UItem.asHeader("Interface"));
+
+        items.add(
+                UItem.asCheck(
+                        hideStoriesRow,
+                        "Hide Stories",
+                        PigeonConfig.hideStories()
+                )
+        );
+
+        items.add(UItem.asShadow(null));
+    }
+
+    private void onItemClick(UItem item,
+                             View view,
+                             int position,
+                             float x,
+                             float y) {
+
+        if (item.id == hideStoriesRow) {
 
             boolean newValue = !PigeonConfig.hideStories();
 
             PigeonConfig.setHideStories(newValue);
 
-            hideStoriesCell.setChecked(newValue);
-        });
-
-        FrameLayout.LayoutParams params =
-                new FrameLayout.LayoutParams(
-                        FrameLayout.LayoutParams.MATCH_PARENT,
-                        FrameLayout.LayoutParams.WRAP_CONTENT
-                );
-
-        params.gravity = Gravity.TOP;
-
-        frameLayout.addView(hideStoriesCell, params);
-
-        fragmentView = frameLayout;
-
-        return fragmentView;
+            adapter.update(true);
+        }
     }
 }
